ae26b48992e11a8ed7ebb8bca20fd509afd530af
Amitabh Prasad <amitabhprasad@Amitabhs-MacBook-Pro.local>
2018-10-08 07:22:33 +0530

    add post Todos route and refactor mangoose

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((results)=>{
+        res.send(results);
+    },(err)=>{
+        res.status(400).send(err);
+    })
+});
+
+app.listen(4501,()=>{
+    console.log('Server started and listening at 4501');
+})
+
+
