f70b273b4ba28f990ac79c44ed214cd1c5b46483
Amitabh Prasad <amitabhprasad@Amitabhs-MacBook-Pro.local>
2018-10-14 21:25:49 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,7 +31,22 @@ app.get('/todos',(req,res)=>{
     },(err)=>{
         res.status(400).send(err);
     });
-})
+});
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send()
+     }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            res.status(404).send(`Requested ID ${id} do not exist`);
+        }
+        res.send(todo);
+    }).catch((err)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(4501,()=>{
     console.log('Server started and listening at 4501');
